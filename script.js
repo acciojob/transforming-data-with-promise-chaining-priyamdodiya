441cd03ae8dde96f80a3f625f0f93e9d0949fe60
priyamdodiya <126169432+priyamdodiya@users.noreply.github.com>
2025-03-06 10:43:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-//your JS code here. If required.
+// your JS code here
+document.getElementById("btn").onclick = function () {
+  const input = document.getElementById("ip").value;
+  const output = document.getElementById("output");
+
+  if (input === "") {
+    alert("Please enter a number");
+    return;
+  }
+
+  let number = parseFloat(input);
+
+  // Initial Promise
+  new Promise((resolve) => {
+    setTimeout(() => {
+      output.innerHTML = `Result: ${number}`;
+      resolve(number);
+    }, 2000);
+  })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          num = num * 2;
+          output.innerHTML = `Result: ${num}`;
+          resolve(num);
+        }, 2000);
+      });
+    })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          num = num - 3;
+          output.innerHTML = `Result: ${num}`;
+          resolve(num);
+        }, 1000);
+      });
+    })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          num = num / 2;
+          output.innerHTML = `Result: ${num}`;
+          resolve(num);
+        }, 1000);
+      });
+    })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          num = num + 10;
+          output.innerHTML = `Final Result: ${num}`;
+          resolve(num);
+        }, 1000);
+      });
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+    });
+};
